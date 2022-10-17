b223efda01dfc48fab1d675de7f2499633704ecc
l1lk <61931009+l1lk@users.noreply.github.com>
2022-10-17 19:56:38 +0800

    Delete generate_demo.sh

--- generate_demo.sh
+++
@@ -1,9 +0,0 @@
-python AttackSet.py   -i ./seeds/mnist/seed_v1/class_0_seed.npy  -o GA_output/GA_100_logits_mnist_lenet4/100_50  -pop_num 100  -subtotal 50  -type mnist -model profile/lenet4_mnist.h5  -target 1  -max_iteration 30
-# python AttackSet.py   -i ./seeds/mnist/seed_v1/class_0_seed.npy  -o GA_output/GA_100_logits_mnist_lenet4/100_50  -pop_num 100  -subtotal 50  -type mnist -model profile/lenet4_mnist.h5  -target 2  -max_iteration 30
-# python AttackSet.py   -i ./seeds/mnist/seed_v1/class_0_seed.npy  -o GA_output/GA_100_logits_mnist_lenet4/100_50  -pop_num 100  -subtotal 50  -type mnist -model profile/lenet4_mnist.h5  -target 3  -max_iteration 30
-# python AttackSet.py   -i ./seeds/mnist/seed_v1/class_0_seed.npy  -o GA_output/GA_100_logits_mnist_lenet4/100_50  -pop_num 100  -subtotal 50  -type mnist -model profile/lenet4_mnist.h5  -target 4  -max_iteration 30
-# python AttackSet.py   -i ./seeds/mnist/seed_v1/class_0_seed.npy  -o GA_output/GA_100_logits_mnist_lenet4/100_50  -pop_num 100  -subtotal 50  -type mnist -model profile/lenet4_mnist.h5  -target 5  -max_iteration 30
-# python AttackSet.py   -i ./seeds/mnist/seed_v1/class_0_seed.npy  -o GA_output/GA_100_logits_mnist_lenet4/100_50  -pop_num 100  -subtotal 50  -type mnist -model profile/lenet4_mnist.h5  -target 6  -max_iteration 30
-# python AttackSet.py   -i ./seeds/mnist/seed_v1/class_0_seed.npy  -o GA_output/GA_100_logits_mnist_lenet4/100_50  -pop_num 100  -subtotal 50  -type mnist -model profile/lenet4_mnist.h5  -target 7  -max_iteration 30
-# python AttackSet.py   -i ./seeds/mnist/seed_v1/class_0_seed.npy  -o GA_output/GA_100_logits_mnist_lenet4/100_50  -pop_num 100  -subtotal 50  -type mnist -model profile/lenet4_mnist.h5  -target 8  -max_iteration 30
-# python AttackSet.py   -i ./seeds/mnist/seed_v1/class_0_seed.npy  -o GA_output/GA_100_logits_mnist_lenet4/100_50  -pop_num 100  -subtotal 50  -type mnist -model profile/lenet4_mnist.h5  -target 9  -max_iteration 30
